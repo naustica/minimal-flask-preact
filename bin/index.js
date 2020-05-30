21ec4060ba27227512469508eace0b3b6fa1a86b
Nick <37081556+naustica@users.noreply.github.com>
2020-05-30 22:40:07 +0200

    update cli

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,58 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
+const yargs = require('yargs');
 const path = require('path');
-const fs = require("fs-extra");
+const fs = require('fs-extra');
 
-const options = yargs
- .usage("Usage: minimal-flask-preact create <project-name>")
- .option("n", { alias: "name", describe: "The application name", type: "string", demandOption: true })
- .locale('en')
- .argv;
 
-// name of project
-const name = options.name;
-// get current working directory
-const dir = process.cwd();
+const argvs = yargs
+  .usage('Usage: minimal-flask-preact create <project-name>')
+  .locale('en')
+  .showHelpOnFail(true)
+  .command('create [name]', 'create a new project', (yargs) => {
+    yargs
+    .positional('name', {
+      describe: 'the application name',
+      type: 'string'
+    })
+  }, (argv) => {
+    // name of project
+    const name = argv.name;
+    // get current working directory
+    const dir = process.cwd();
 
-// destination
-const destination = path.join(dir, name);
+    // destination
+    const destination = path.join(dir, name);
 
-// get current working directory of this package
-const package_dir = __dirname;
+    // get current working directory of this package
+    const package_dir = __dirname;
 
-fs.copy(path.join(package_dir, 'app'), path.join(destination), function (err) {
-  if (err) return console.log(err)
-})
+    fs.copy(path.join(package_dir, 'app'), path.join(destination), (err) => {
+      if (err) return console.log(err)
+    })
+
+    console.log('Finished')
+    }
+  )
+  .command({
+    command: '*',
+    handler: () => {
+      yargs.showHelp()
+    }
+  })
+  .options({
+    'v': {
+      alias: 'version',
+      describe: 'show version'
+    },
+    'h': {
+      alias: 'help',
+      describe: 'show help'
+    }
+  })
+  .help()
+  .fail((msg) => {
+    console.error(msg.red);
+    yargs.showHelp();
+  })
+  .argv;
